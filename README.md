7daec429cd67dbdbccb8dd2b9691894babc8d254
Niky Piky <naldorsc2@gmail.com>
2023-01-07 20:17:13 +0100

    initial setup and writing of dates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+this is the birthday paradox from the python The Big Book of Small Python Projects - https://inventwithpython.com/bigbookpython/project2.html
 # birthday-paradox
---
+++ birthday-paradox.py
@@ -0,0 +1,20 @@
+import math
+
+nmbr_of_birthdays = input('How many birthdays shal I generate?')
+print(nmbr_of_birthdays)
+
+dates = ['January', 31, 'February', 28, 'March', 31, 'April', 30, 'May', 31, 'June', 30, 'July', 31, 'August', 31, 'September', 30, 'October', 31, 'November', 30, 'December', 31]
+
+1	January	31 days
+2	February	28 days (common year)
+29 days (leap year*)
+3	March	31 days
+4	April	30 days
+5	May	31 days
+6	June	30 days
+7	July	31 days
+8	August	31 days
+9	September	30 days
+10	October	31 days
+11	November	30 days
+12	December	31 days
