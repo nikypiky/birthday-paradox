06040184243f03ab9c76d92980c3a82a3c301795
Niky Piky <naldorsc2@gmail.com>
2023-01-08 20:54:25 +0100

    seting up random choosing of dates

--- birthday-paradox.py
+++ birthday-paradox.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 def request_number():
     while True:
@@ -9,11 +10,17 @@ def request_number():
             print("Invalid input. Please choose a number between 1-100.")
 
 # Use the function to request a number from the user
-#number = request_number()
+number = request_number()
 
 # Print the number outside of the function
-print(request_number()())
-
-
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#dates = {'January' : 31, 'February' : 28, 'March' : 31, 'April' : 30, 'May' : 31, 'June' : 30, 'July' : 31, 'August' : 31, 'September' : 30, 'October' : 31, 'November' : 30, 'December' : 31}
+#for n in number:
+index_dates = random.randint(0, 11)
+print(index_dates)
+print(months[index_dates])
+print(days[index_dates])
+
+
+
