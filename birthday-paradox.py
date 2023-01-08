4e6127cc4b627fc612cf663a935312452958caf5
Niky Piky <naldorsc2@gmail.com>
2023-01-08 13:18:43 +0100

    creating function for user request + days list

--- birthday-paradox.py
+++ birthday-paradox.py
@@ -1,20 +1,15 @@
 import math
+def request_nmbr():
+    nmbr_of_birthdays = input('How many birthdays shal I generate? ')
+    try:
+        nmbr_of_birthdays = int(nmbr_of_birthdays)
+    except:
+        print('Plese choose a number')
+        request_nmbr()
+        print()
+    print(nmbr_of_birthdays)
+    
+request_nmbr()
 
-nmbr_of_birthdays = input('How many birthdays shal I generate?')
-print(nmbr_of_birthdays)
-
-dates = ['January', 31, 'February', 28, 'March', 31, 'April', 30, 'May', 31, 'June', 30, 'July', 31, 'August', 31, 'September', 30, 'October', 31, 'November', 30, 'December', 31]
-
-1	January	31 days
-2	February	28 days (common year)
-29 days (leap year*)
-3	March	31 days
-4	April	30 days
-5	May	31 days
-6	June	30 days
-7	July	31 days
-8	August	31 days
-9	September	30 days
-10	October	31 days
-11	November	30 days
-12	December	31 days
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
