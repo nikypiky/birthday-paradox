c45361ed2ee89bcaf839ed1c19c342ec50d93a74
Niky Piky <naldorsc2@gmail.com>
2023-01-08 17:00:03 +0100

    seting user input to accept only numbers 1-100

--- birthday-paradox.py
+++ birthday-paradox.py
@@ -1,15 +1,19 @@
 import math
-def request_nmbr():
-    nmbr_of_birthdays = input('How many birthdays shal I generate? ')
-    try:
-        nmbr_of_birthdays = int(nmbr_of_birthdays)
-    except:
-        print('Plese choose a number')
-        request_nmbr()
-        print()
-    print(nmbr_of_birthdays)
-    
-request_nmbr()
+
+def request_number():
+    while True:
+        input_str = input("How many birthdays shal I generate? ")
+        if input_str.isnumeric() and int(input_str)<=100 and int(input_str)>0:
+            return int(input_str)        
+        else:
+            print("Invalid input. Please choose a number between 1-100.")
+
+# Use the function to request a number from the user
+#number = request_number()
+
+# Print the number outside of the function
+print(request_number()())
+
 
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
